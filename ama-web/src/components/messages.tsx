7639562be3aef7835fc3bf12635608dcde69fbff
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-08-08 11:00:44 -0300

    feat: created web app

---
+++ ama-web/src/components/messages.tsx
@@ -0,0 +1,36 @@
+import { useParams } from "react-router-dom";
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { Message } from "./message";
+import { getRoomMessages } from "../http/get-room-messages";
+import { useMessagesWebSockets } from "../hooks/use-messages-web-sockets";
+
+export function Messages() {
+  const { roomId } = useParams()
+
+  if (!roomId) {
+    throw new Error('Messages component must be used within rooom page')
+  }
+
+  const { data } = useSuspenseQuery({
+    queryKey: ['messages', roomId],
+    queryFn: () => getRoomMessages({ roomId }),
+  })
+
+  useMessagesWebSockets({ roomId })
+
+  const sortedMessages = data.messages.sort((a, b) => b.amountOfReactions - a.amountOfReactions)
+
+  return (
+    <ol className="list-decimal list-inside px-3 space-y-8">
+      {sortedMessages.map(message => (
+        <Message
+          key={message.id}
+          messageId={message.id}
+          text={message.text}
+          amountOfReactions={message.amountOfReactions} 
+          answered={message.answered}
+        />
+      ))}
+    </ol>
+  )
+}
